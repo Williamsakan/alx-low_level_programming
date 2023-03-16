bedef8a525c79c75d162dfcaf9dea377786d907e
Williamsakan <akan.williams@outlook.com>
2023-03-16 15:56:10 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+	* main - Entry point
+	* description: program that prints using printf followed by a new line
+	* Return: Always 0 (success)
+	*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
