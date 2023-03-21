41b6bc844b40182f04d89052c8672aa0bef7c6bc
Williamsakan <akan.williams@outlook.com>
2023-03-21 05:20:45 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+	* main - prints putchar
+	* Return: always 0
+	*/
 int main(void)
 {
 	_putchar('_');
@@ -17,5 +16,6 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
-}
+ 	return (0);
+ }
+
